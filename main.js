2d77ab975c7909d3e38eb89aec55cf6c5f8cb74f
Nino Chimento <saninoc84@gmail.com>
2020-01-31 12:56:46 +0100

    richiamo e carico tutti i dischi dal server con un ciclo for

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	//Code
+	$.ajax({
+   "url": "https://flynn.boolean.careers/exercises/api/array/music",
+   "method": "GET",
+   success :function (data) {
+     console.log(data.response.length);
+     for (var i = 0; i < data.response.length; i++) {
+       var source = $("#entry-template").html();
+       var template = Handlebars.compile(source);
+       var context = data.response[i];
+       var html = template(context);
+       $(".cds-container").append(html);
+     }
+   },
+   error: function (richiesta,stato,errore) {
+     alert("si e verificato l errore"+ errore)
+   }
+  });
 });
