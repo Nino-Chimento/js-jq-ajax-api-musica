a2383ad6bd8f405ff468df9a915dbede3b05caef
Nino Chimento <saninoc84@gmail.com>
2020-01-31 13:55:34 +0100

    alla nuova ricerca interrogo nuovamente il database

--- main.js
+++ main.js
@@ -15,8 +15,27 @@ $(document).ready(function() {
      alert("si e verificato l errore"+ errore)
    }
   });
-  $("select").click(function () {
+  $("button").click(function () {
     var selezione = $("select").val();
     $(".container > div").remove();
+    $.ajax({
+     "url": "https://flynn.boolean.careers/exercises/api/array/music",
+     "method": "GET",
+     success :function (data) {
+       for (var i = 0; i < data.response.length; i++) {
+         if (data.response[i].genre == selezione) {
+           var source = $("#entry-template").html();
+           var template = Handlebars.compile(source);
+           var context = data.response[i];
+           var html = template(context);
+           $(".cds-container").append(html);
+
+         }
+       }
+     },
+     error: function (richiesta,stato,errore) {
+       alert("si e verificato l errore"+ errore)
+     }
+    });
   })
 });
